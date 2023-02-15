0170838e6914d72b64dbbdd4b783be19e9bd5e8d
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-02-15 21:50:03 +0300

    Done with the footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,9 @@
 <template>
   <q-page class="flex justify-evenly space-y-8">
+    <q-parallax
+      src="https://www.beekeepingfornewbies.com/wp-content/uploads/2019/09/Honey-and-Honeycomb.jpg"
+      :height="100"
+    />
     <q-img
       src="~assets/honey-main.png"
       spinner-color="brown"
@@ -12,7 +16,6 @@
       </q-btn>
       <!-- </div> -->
     </q-img>
-
     <div class="flex column text-center content-around space-y-5">
       <div>
         <p class="my-font self-center text-h2 q-mx-md">
@@ -24,7 +27,7 @@
           >lden.
         </p>
       </div>
-
+      <FloatButton />
       <div>
         <q-btn
           outline
@@ -40,127 +43,85 @@
     </div>
     <!-- ============ End First Section ====================== -->
 
-    <div class="flex flex-col space-y-4">
-      <div>
+    <div class="flex flex-col">
+      <div class="row q-mb-md">
         <q-img src="~assets/products.png" class="q-ml-md" width="2em" />
+        <p class="q-ml-md -mb-[8px] text-h3 text-weight-bolder text-amber-9">
+          Honey
+        </p>
       </div>
+
       <div>
-        <p class="q-ml-md text-h3 text-weight-bolder text-amber-9">Honey</p>
-      </div>
-      <div>
-        <p class="self-center text-h6 q-mx-md">
-          is a one-ingredient recipe made by bees, flowers and mother nature
+        <q-parallax :height="250">
+          <template v-slot:media>
+            <video
+              width="720"
+              height="500"
+              poster="https://assets.mixkit.co/videos/preview/mixkit-honey-dipper-with-honey-2802-large.mp4"
+              autoplay
+              loop
+              muted
+            >
+              <source
+                type="video/webm"
+                src="https://assets.mixkit.co/videos/preview/mixkit-honey-dipper-with-honey-2802-large.mp4"
+              />
+              <source
+                type="video/mp4"
+                src="hhttps://assets.mixkit.co/videos/preview/mixkit-honey-dipper-with-honey-2802-large.mp4"
+              />
+            </video>
+          </template>
+        </q-parallax>
+
+        <p class="self-center text-h6 q-mx-md text-weight-bolder">
+          Is a one-ingredient recipe made by bees, flowers and mother nature
           herself.
         </p>
       </div>
-      <FloatButton />
-      <div>
+
+      <div class="-mt-[20px]">
         <q-btn
           color="primary"
-          icon-right="send"
+          icon-left="hive"
           label="Learn About Honey"
-          class="q-ml-md mt-10"
+          class="q-mt-md"
+          flat
         />
       </div>
 
       <div><q-separator color="orange" spaced="16px" inset /></div>
     </div>
-    <q-separator color="orange" spaced="16px" inset />
+    <q-separator color="orange" spaced="5px" inset />
 
-    <div class="w-full space-y-8">
-      <q-carousel
-        v-model="slide"
-        transition-prev="slide-right"
-        transition-next="slide-left"
-        swipeable
-        animated
-        control-color="primary"
-        navigation
-        padding
-        arrows
-        height="350px"
-        class="bg-grey-1 shadow-2 rounded-borders w-full transparent"
-      >
-        <q-carousel-slide :name="1" class="column no-wrap">
-          <h3 class="text-h5 text-weight-bolder text-amber-9 q-pb-sm">
-            Modern Manufacture
-          </h3>
-
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://www.efsa.europa.eu/sites/default/files/styles/lg_col_12_16x9/public/news/bees_beekeeper.jpg?h=91d67b52&itok=jYzprNF2"
-            />
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://media.istockphoto.com/id/623103036/photo/hives-in-an-apiary-with-bees-flying-to-the-landing.jpg?s=612x612&w=0&k=20&c=4q6oj20j0JeTG4W1rxLZu_iUX1NyyE9YLXlU_-ohg5g="
-            />
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="2" class="column no-wrap">
-          <h3 class="text-h5 text-weight-bolder text-amber-9 q-pb-sm">
-            100% Natural
-          </h3>
-
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://cdn7.dissolve.com/p/D1062_38_175/D1062_38_175_1200.jpg"
-            />
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://images.immediate.co.uk/production/volatile/sites/4/2020/09/GettyImages-1204957609-crop-cfd245a.jpg"
-            />
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="3" class="column no-wrap">
-          <h3 class="text-h5 text-weight-bolder text-amber-9 q-pb-sm">
-            Batch Production
-          </h3>
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd0jnpcOYs5uVWogKPnHSOghifZ0bjwfyO0Mf--ZljZzKo-_8D99-atEnUQ-yd-qhas4s&usqp=CAU"
-            />
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu7_dYcg4d5_BXtU_yi9lwxZzVVYLfuZhI6YnbhgdyBhAts-WI43jbg7LOtBC3XFyvRGE&usqp=CAU"
-            />
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="4" class="column no-wrap">
-          <h3 class="text-h5 text-weight-bolder text-amber-9 q-pb-sm">
-            Fast Delivery
-          </h3>
-
-          <div
-            class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
-          >
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://www.shutterstock.com/image-vector/big-truck-honey-smiling-men-260nw-1150435877.jpg"
-            />
-            <q-img
-              class="rounded-borders col-6 full-height"
-              src="https://thumbs.dreamstime.com/z/honey-truck-icon-isometric-honey-truck-vector-icon-web-design-isolated-white-background-honey-truck-icon-isometric-style-121678197.jpg"
-            />
-          </div>
-        </q-carousel-slide>
+    <div class="w-full">
+      <q-carousel animated v-model="slide" arrows navigation infinite>
+        <q-carousel-slide
+          :name="1"
+          img-src="https://cdn7.dissolve.com/p/D1062_38_175/D1062_38_175_1200.jpg"
+        />
+        <q-carousel-slide
+          :name="2"
+          img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd0jnpcOYs5uVWogKPnHSOghifZ0bjwfyO0Mf--ZljZzKo-_8D99-atEnUQ-yd-qhas4s&usqp=CAU"
+        />
+        <q-carousel-slide
+          :name="3"
+          img-src="https://media.istockphoto.com/id/623103036/photo/hives-in-an-apiary-with-bees-flying-to-the-landing.jpg?s=612x612&w=0&k=20&c=4q6oj20j0JeTG4W1rxLZu_iUX1NyyE9YLXlU_-ohg5g="
+        />
+        <q-carousel-slide
+          :name="4"
+          img-src="https://www.shutterstock.com/image-vector/big-truck-honey-smiling-men-260nw-1150435877.jpg"
+        />
       </q-carousel>
-
-      <q-separator color="orange" spaced="16px" inset />
-      <q-btn
-        color="primary"
-        icon-right="send"
-        label="Learn About Honey"
-        class="mx-4"
-      />
+      <div class="-mt-[50px]">
+        <q-btn
+          color="primary"
+          icon-left="hive"
+          label="Our Quality Products"
+          class="q-mt-md"
+          flat
+        />
+      </div>
     </div>
   </q-page>
 </template>
@@ -178,3 +139,9 @@ export default {
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 500px
+</style>
