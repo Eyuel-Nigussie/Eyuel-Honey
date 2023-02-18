5a3b88f9486b872bfea12bca9e190ae4420578a9
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-02-18 16:44:43 +0300

    UI adjustment

--- src/pages/ProductConfirm.vue
+++ src/pages/ProductConfirm.vue
@@ -1,6 +1,6 @@
 <template>
-  <qpage class="flex flex-col space-y-20 mx-auto">
-    <div class="mt-[200px] mx-auto">
+  <qpage class="flex flex-col space-y-10 mx-auto">
+    <div class="mt-[100px] mx-auto">
       <h1 class="text-[50px] font-bold">Congratulation!</h1>
       <div class="ml-20">
         <p class="font-bold text-[20px]">Your order is on it's way</p>
@@ -15,16 +15,15 @@
     <div
       class="border-solid border-2 border-slate-900 p-5 mx-10 flex flex-col space-y-20"
     >
-      <p class="mx-auto font-bold text-[20px] text-center">
-        Tell your friends family and loved once about our greate life choice:
-      </p>
+      <div>
+        <p class="mx-auto font-bold text-[20px] text-center">
+          Tell your friends family and loved once about our greate life choice:
+        </p>
+      </div>
       <div class="flex flex-row mx-auto space-x-4">
         <q-icon name="facebook" size="60px" class="-ml-[10px] -mt-[60px]" />
         <q-icon name="flutter_dash" size="60px" class="-ml-[10px] -mt-[60px]" />
         <q-icon name="email" size="60px" class="-ml-[10px] -mt-[60px]" />
-      </div>
-      <div>
-        <!-- icons -->
       </div>
     </div>
   </qpage>
